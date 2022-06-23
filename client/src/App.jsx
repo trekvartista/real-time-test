4f30de27def2ae85f78dfe6dc04bc4053e5f7025
trekvartista <isken_150502@mail.ru>
2022-06-23 18:54:23 +0600

    changed start script and app render component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import EventSourcing from "./components/EventSourcing";
 import LongPolling from "./components/LongPolling";
+import WebSocketTest from "./components/WebSocketTest";
 
 function App() {
     
     return (
-        <EventSourcing />
+        <WebSocketTest />
     );
 }
 
